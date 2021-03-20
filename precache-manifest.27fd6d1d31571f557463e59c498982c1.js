b58cb9a9c9df25b864df70fdde8806f06cca306f
zzunebye <bhang96@gmail.com>
2021-03-20 15:38:24 +0800

    Updates

---
+++ precache-manifest.27fd6d1d31571f557463e59c498982c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eff2935e90c80c3ac2c75f687904ab0",
+    "url": "/react-tailwind-css-image-gallery/index.html"
+  },
+  {
+    "revision": "26e48bf9a4c2f563c753",
+    "url": "/react-tailwind-css-image-gallery/static/css/main.f3c6f3f5.chunk.css"
+  },
+  {
+    "revision": "f21cac6b32ef7d53c248",
+    "url": "/react-tailwind-css-image-gallery/static/js/2.0bda55f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tailwind-css-image-gallery/static/js/2.0bda55f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e48bf9a4c2f563c753",
+    "url": "/react-tailwind-css-image-gallery/static/js/main.80671947.chunk.js"
+  },
+  {
+    "revision": "f98bfff567843bff3ba4",
+    "url": "/react-tailwind-css-image-gallery/static/js/runtime-main.92ed2de3.js"
+  }
+]);
